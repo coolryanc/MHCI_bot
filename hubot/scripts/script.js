4fb610ee39b7366f4230cbc6c44a40cbdb2be44c
ryanC1993 <tommy149347@gmail.com>
2017-02-17 01:04:56 +0800

    + del func()

--- hubot/scripts/script.js
+++ hubot/scripts/script.js
@@ -8,10 +8,10 @@ module.exports = function(robot){
     });
 
 
-   // ============
+   	// ============
 	// ledger info
 	// ============
-	robot.hear(/記帳 (.*)|記帳本/i,function(res){
+	robot.hear(/記帳 (.+)$|記帳本/i,function(res){
 
 		// load js module
 		var fsdata = require('fs');
@@ -26,12 +26,11 @@ module.exports = function(robot){
 				if(err){
 					res.reply("Sorry, my bad.."); // can't read data
 				}
-				else{
-					
+				else{	
 					let jsonData = JSON.parse(data);
 					let replyStr = "";
 					jsonData.forEach( function(item, index) {
-						let record = (index+1).toString()+". "+item.registerPerson+" say: "+item.say+"\n";
+						let record = (index+1).toString()+". "+item.registerPerson+" say: "+item.say+"  "+item.date+"\n";
 						replyStr +=  record;
 					});
 					res.reply("`記帳本`\n>```"+replyStr+"```");
@@ -50,9 +49,12 @@ module.exports = function(robot){
 
 					// record info
 					let jsonData = JSON.parse(data);
-					let pushData = {"registerPerson":"", "say":""};
+					let pushData = {"registerPerson":"", "say":"", "date":""};
+					let today = new Date();
+					today = today.getMonth()+1 +'/'+ today.getDate() +'/'+ today.getFullYear();
 					pushData.registerPerson = res.message.user.name;
 					pushData.say = content;
+					pushData.date = today;
 					jsonData.push(pushData);
 
 					//write back data
@@ -68,6 +70,49 @@ module.exports = function(robot){
 			});// End read file
 		}
 	});
+
+	robot.hear(/刪帳 (.*)/i, function(res){
+        // load js module
+		var fsdata = require('fs');
+		var path = require('path');
+
+		// get data path 
+		let filepath = path.join(__dirname, 'data', 'accounting.txt')
+		if (!parseInt(res.match[1])){
+			res.reply("刪帳後面接的是 `阿拉伯數字` 喔！");
+		}	
+
+		var delIndex = parseInt(res.match[1])-1;
+		fsdata.readFile(filepath,'utf8',function(err,data){
+			if(err){
+				res.reply("Sorry, my bad.."); // can't read data
+			}
+			else{
+
+				// record info
+				let jsonData = JSON.parse(data);
+				let delMan = res.message.user.name;
+				if (delMan !== jsonData[delIndex].registerPerson){
+					res.reply("不要亂刪別人的帳！！！");
+				}
+				
+				else {
+					jsonData.splice(delIndex,1);
+					//write back data
+					fsdata.writeFile(filepath, JSON.stringify(jsonData), function(err){
+						if(err){
+							res.reply("I can't record...");
+						}
+						else{
+							res.reply("好囉！幫你刪掉囉，可以打 `記帳本` 去看帳本");
+						}
+					});// End write file
+				}
+			} 
+		});// End read file
+
+    });
+
 
 
 	// ============
